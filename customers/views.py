d0cc2d0086ae513a6a8098ed5b443265d33687ac
R-Prince <reneprince93@icloud.com>
2021-03-11 22:39:16 +0000

    Add customers template and view

--- customers/views.py
+++ customers/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from .models import Customer
 
 
 # View users customers
 def customers(request):
+    customers = get_list_or_404(Customer, user=request.user)
     template = 'customers/customers.html'
     context = {
+        'customers': customers
     }
     return render(request, template, context)
