c27d27276a3dc176b110b270815bb46b6d9418db
R-Prince <reneprince93@icloud.com>
2021-03-21 22:44:02 +0000

    Add basic stripe checkout functionality

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -3,22 +3,32 @@ from .models import Subscription
 from profiles.models import UserProfile
 from django.contrib.auth.decorators import login_required
 from datetime import date, timedelta
+from django.conf import settings
+import stripe
 
 
 # Update Users Subscription
 @login_required
 def update_subscription(request):
-
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     current_subscription = get_object_or_404(Subscription, user=request.user)
     profile = get_object_or_404(UserProfile, user=request.user)
     sub_end = date.today() + timedelta(days=31)
+    current_sub_price = 1999
+
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=current_sub_price,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     context = {
         'current_subscription': current_subscription,
         'profile': profile,
         'sub_end': sub_end,
-        'stripe_public_key': 'pk_test_51IXY2lAeZyzq5JP9qRM2UnKZnKMGIbrb5z5XeTBPRkx6sKyxFFxvtmfmKhbB7Y0uAoPUlylpF0AUP4uVmPg33SI90047fwc770',
-        'client_secret': 'test secret key'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret
     }
     template = 'subscription/update_subscription.html'
     return render(request, template, context)
