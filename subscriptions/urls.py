dd11c08e1e01c9093178e43de842b763c255ece7
R-Prince <reneprince93@icloud.com>
2021-03-23 23:12:48 +0000

    Add class methods to webhook handler and webhook view

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('update_sub', views.update_subscription, name='update_sub'),
     path(
         'checkout_success/',
         views.checkout_success, name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
