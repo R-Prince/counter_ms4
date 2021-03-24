643c2a4a0f320bf5406b97b5b7bb105da8144df5
R-Prince <reneprince93@icloud.com>
2021-03-24 23:24:35 +0000

    Add cache checkout view to handle stripe meta data

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path(
         'checkout_success/',
         views.checkout_success, name='checkout_success'),
+    path(
+        'cache_checkout_data/',
+        views.cache_checkout_data,
+        name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
 ]
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.shortcuts import render, get_object_or_404, reverse, redirect, HttpResponse
+from django.views.decorators.http import require_POST
 from .models import Subscription
 from profiles.models import UserProfile
 from django.contrib.auth.decorators import login_required
@@ -6,6 +7,21 @@ from datetime import date, timedelta
 from django.conf import settings
 from django.contrib import messages
 import stripe
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 # Update Users Subscription
