9c64e8da9c24b15df8485e96c3ef6f1221217203
R-Prince <reneprince93@icloud.com>
2021-03-31 02:52:21 +0000

    Remove secret key and set debug

--- counter/settings.py
+++ counter/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'is7gg5qp=e%-n2k3!@#x0k1+s7&pk+0wo#9b8^g3w!iwp%*vu('
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['counter-ms4.herokuapp.com', 'localhost']
 
